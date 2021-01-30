f187678a5b48efc742a085406ab05b4c6473ba98
jisung <jisung@The-Air.local>
2021-01-30 15:54:04 +0900

    initial commit

---
+++ post.py
@@ -0,0 +1,9 @@
+class Post:
+    def __init__(self, post_id, title,subtitle, body,image_url, author, date):
+        self.id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
+        self.image_url = image_url
+        self.author = author
+        self.date = date
